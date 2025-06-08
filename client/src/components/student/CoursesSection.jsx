6840d9e61c5094471e5a0c2cb38322b0a8c22737
padmesh19 <padmesh191101@gmail.com>
2025-06-08 23:44:17 +0530

    course section alignment change

--- client/src/components/student/CoursesSection.jsx
+++ client/src/components/student/CoursesSection.jsx
@@ -17,7 +17,7 @@ const CoursesSection = () => {
         deliver results.
       </p>
 
-      <div className="grid grid-auto px-4 md:px-0 md:my-16 my-10 gap-4">
+      <div className="grid justify-center sm:grid-cols-[repeat(auto-fit,_minmax(0,_350px))] px-4 md:px-0 md:my-16 my-10 gap-4">
         {allCourses.slice(0, 4).map((course, index) => (
           <CourseCard key={index} course={course} />
         ))}
